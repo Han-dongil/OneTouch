a2ff19f9f93484073052355512d05569a14c1b48
gwgtis135 <46915671+gwgtis135@users.noreply.github.com>
2022-01-13 17:39:25 +0900

    2022-01-13 17:38 설비 수리관리 페이지 수정할 때 업데이트 부분 기능 오류 종현

--- oneTouch/src/main/java/com/onetouch/web/fct/fix/dao/FixVO.java
+++ oneTouch/src/main/java/com/onetouch/web/fct/fix/dao/FixVO.java
@@ -13,16 +13,16 @@ public class FixVO {
 
 	String fixCd;
 	String fctCd;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm ", timezone = "Asia/Seoul")
 	Date reqDt;		//입고일	
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
 	Date strDt;		//입고일	
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
 	Date finDt;		//입고일
-	String fixPhs;
+	String fixPhs;				//수리상태 "수리요청", "수리중", "수리완료"
 	String fixCmt;
 	int  cnt;
 	int unitCost;
