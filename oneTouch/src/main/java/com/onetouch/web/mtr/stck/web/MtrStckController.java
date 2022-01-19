e8dde7b7f317840fdcf15613f1dd92e98002a2d1
Jacob <ehrhs03@gmail.com>
2022-01-19 10:09:11 +0900

    2022-01-19 10:09 kdg

--- oneTouch/src/main/java/com/onetouch/web/mtr/stck/web/MtrStckController.java
+++ oneTouch/src/main/java/com/onetouch/web/mtr/stck/web/MtrStckController.java
@@ -1,5 +1,25 @@
 package com.onetouch.web.mtr.stck.web;
 
+import org.springframework.stereotype.Controller;
+@Controller
 public class MtrStckController {
 
+//	@Autowired MtrInService service;
+//	
+//	@RequestMapping("inList")
+//	public String inForm() {
+//		return "tiles/mtr/mtrInList";
+//	}
+//	
+//	//조건조회 grid
+//	@ResponseBody
+//	@PostMapping("/mtrInList")
+//	public Map<String,Object> selectInList(@RequestBody MtrSearchVO searchVO){
+//		Map<String,Object> datas = new HashMap<>();
+//		Map<String,Object> data = new HashMap<>();
+//		data.put("result", true);
+//		datas.put("contents", service.selectIn(searchVO));
+//		data.put("data", datas);
+//		return data;
+//	}
 }
