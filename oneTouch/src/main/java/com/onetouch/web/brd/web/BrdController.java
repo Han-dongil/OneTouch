a322a1d7c216524c31356c7d8c6d5fc44d225c3c
Jacob <ehrhs03@gmail.com>
2022-02-03 17:46:09 +0900

    2022-02-03 17:46 kdg

--- oneTouch/src/main/java/com/onetouch/web/brd/web/BrdController.java
+++ oneTouch/src/main/java/com/onetouch/web/brd/web/BrdController.java
@@ -1,7 +1,6 @@
 package com.onetouch.web.brd.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.onetouch.web.brd.service.BrdService;
 @RequestMapping("/brd")
-@Controller
 public class BrdController {
 	
 	@Autowired BrdService service;
