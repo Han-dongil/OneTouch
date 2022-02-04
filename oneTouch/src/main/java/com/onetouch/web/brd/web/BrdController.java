3603b58541b6074d7fc41c816b4d77798eba9623
ehrhs <Ehrhs03@gmail.com>
2022-02-05 03:19:02 +0900

    2022-02-05 03:18 kdg

--- oneTouch/src/main/java/com/onetouch/web/brd/web/BrdController.java
+++ oneTouch/src/main/java/com/onetouch/web/brd/web/BrdController.java
@@ -3,9 +3,7 @@ package com.onetouch.web.brd.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.onetouch.web.brd.service.BrdService;
 @RequestMapping("/brd")
@@ -14,17 +12,17 @@ public class BrdController {
 	
 	@Autowired BrdService service;
 	
-	@RequestMapping("/dashBoard")
-	public String dashBoard() {
-		return "tiles/brd/dashBoard";
-	}
+//	@RequestMapping("/dashBoard")
+//	public String dashBoard() {
+//		return "tiles/brd/dashBoard";
+//	}
 	
-	@ResponseBody
-	@PostMapping("/brdList")
+	@RequestMapping("/dashBoard")
 	public String brdList(Model model){
+		System.out.println(service.mtrList());
 		model.addAttribute("mtrList", service.mtrList());
 		model.addAttribute("fctList", service.fctList());
 		model.addAttribute("pdtList", service.pdtList());
-		return "brd/dashBoard";
+		return "tiles/brd/dashBoard";
 	}
 }
