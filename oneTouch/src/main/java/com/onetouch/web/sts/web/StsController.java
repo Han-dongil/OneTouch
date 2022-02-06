e3b9e74f386ed58f9da2740d1c5641269294d7b9
sunyoung9797 <allena4302@naver.com>
2022-02-07 00:47:44 +0900

    2022-02-07 00:46 선영 통계 페이지 수정

--- oneTouch/src/main/java/com/onetouch/web/sts/web/StsController.java
+++ oneTouch/src/main/java/com/onetouch/web/sts/web/StsController.java
@@ -24,88 +24,190 @@ public class StsController {
 		return "tiles/sts/stsList";
 	}
 	
-	//자재입고량---------------------
-	//일별 조회
-	@ResponseBody
-	@GetMapping("/mtrInDate")
-	public Map<String, Object> mtrInDate(StsVO stsvo) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("contents", stsservice.mtrInDate(stsvo));
-
-		Map<String, Object> maps = new HashMap<>();
-		maps.put("result", true);
-		maps.put("data", map);
-
-		return maps;
-	}
+	//자재입고량--------------------------------------------------------
+		//일별 조회
+		@ResponseBody
+		@GetMapping("/mtrInDate")
+		public Map<String, Object> mtrInDate(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.mtrInDate(stsvo));
 	
-	//월별 조회
-	@ResponseBody
-	@GetMapping("/mtrInMonth")
-	public Map<String, Object> mtrInMonth(StsVO stsvo) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("contents", stsservice.mtrInMonth(stsvo));
-
-		Map<String, Object> maps = new HashMap<>();
-		maps.put("result", true);
-		maps.put("data", map);
-
-		return maps;
-	}
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
 	
-	//연도별 조회
-	@ResponseBody
-	@GetMapping("/mtrInYear")
-	public Map<String, Object> mtrInYear(StsVO stsvo) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("contents", stsservice.mtrInYear(stsvo));
-
-		Map<String, Object> maps = new HashMap<>();
-		maps.put("result", true);
-		maps.put("data", map);
-
-		return maps;
-	}
+			return maps;
+		}
+		
+		//월별 조회
+		@ResponseBody
+		@GetMapping("/mtrInMonth")
+		public Map<String, Object> mtrInMonth(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.mtrInMonth(stsvo));
 	
-	//자재별 , 일별 조회
-	@ResponseBody
-	@GetMapping("/mtrInMtrD")
-	public Map<String, Object> mtrInMtrD(StsVO stsvo) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("contents", stsservice.mtrInMtrD(stsvo));
-
-		Map<String, Object> maps = new HashMap<>();
-		maps.put("result", true);
-		maps.put("data", map);
-
-		return maps;
-	}
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
 	
-	//자재별 , 월별 조회
-	@ResponseBody
-	@GetMapping("/mtrInMtrM")
-	public Map<String, Object> mtrInMtrM(StsVO stsvo) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("contents", stsservice.mtrInMtrM(stsvo));
-
-		Map<String, Object> maps = new HashMap<>();
-		maps.put("result", true);
-		maps.put("data", map);
-
-		return maps;
-	}
+			return maps;
+		}
+		
+		//연도별 조회
+		@ResponseBody
+		@GetMapping("/mtrInYear")
+		public Map<String, Object> mtrInYear(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.mtrInYear(stsvo));
 	
-	//자재별 , 연도별 조회
-	@ResponseBody
-	@GetMapping("/mtrInMtrY")
-	public Map<String, Object> mtrInMtrY(StsVO stsvo) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("contents", stsservice.mtrInMtrY(stsvo));
-
-		Map<String, Object> maps = new HashMap<>();
-		maps.put("result", true);
-		maps.put("data", map);
-
-		return maps;
-	}
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//자재별 , 일별 조회
+		@ResponseBody
+		@GetMapping("/mtrInMtrD")
+		public Map<String, Object> mtrInMtrD(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.mtrInMtrD(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//자재별 , 월별 조회
+		@ResponseBody
+		@GetMapping("/mtrInMtrM")
+		public Map<String, Object> mtrInMtrM(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.mtrInMtrM(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//자재별 , 연도별 조회
+		@ResponseBody
+		@GetMapping("/mtrInMtrY")
+		public Map<String, Object> mtrInMtrY(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.mtrInMtrY(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+	
+	
+	//자재출고량--------------------------------------------------------
+		//일별 조회
+		//월별 조회
+		//연도별 조회
+		//자재별,일별 조회
+		//자재별,월별 조회
+		//자재별,연도별 조회
+		
+	//제품생산량--------------------------------------------------------
+		//일별 조회
+		@ResponseBody
+		@GetMapping("/pdtDate")
+		public Map<String, Object> pdtDate(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.pdtDate(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//월별 조회
+		@ResponseBody
+		@GetMapping("/pdtMonth")
+		public Map<String, Object> pdtMonth(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.pdtMonth(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//연도별 조회
+		@ResponseBody
+		@GetMapping("/pdtYear")
+		public Map<String, Object> pdtYear(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.pdtYear(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//자재별,일별 조회
+		@ResponseBody
+		@GetMapping("/pdtPrdD")
+		public Map<String, Object> pdtPrdD(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.pdtPrdD(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//자재별,월별 조회
+		@ResponseBody
+		@GetMapping("/pdtPrdM")
+		public Map<String, Object> pdtPrdM(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.pdtPrdM(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+		//자재별,연도별 조회
+		@ResponseBody
+		@GetMapping("/pdtPrdY")
+		public Map<String, Object> pdtPrdY(StsVO stsvo) {
+			Map<String, Object> map = new HashMap<>();
+			map.put("contents", stsservice.pdtPrdY(stsvo));
+	
+			Map<String, Object> maps = new HashMap<>();
+			maps.put("result", true);
+			maps.put("data", map);
+	
+			return maps;
+		}
+		
+	//제품불량량--------------------------------------------------------
+		//일별 조회
+		//월별 조회
+		//연도별 조회
+		//자재별,일별 조회
+		//자재별,월별 조회
+		//자재별,연도별 조회
 }
